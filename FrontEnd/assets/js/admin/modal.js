78c35dd0a0b506787d13e197150472887724fa98
Phanttoms <yohan.pruche@orange.fr>
2023-12-18 13:03:04 +0100

    ajout gallery dans la modal

--- FrontEnd/assets/js/admin/modal.js
+++ FrontEnd/assets/js/admin/modal.js
@@ -1,11 +1,35 @@
 // Récuperation éléments modal
 const myModal = document.getElementById("modalContainer");
+const allDocBackground = document.getElementById("allDoc");
 
 // Fonction d'ouverture ou fermeture de la modal
 export async function openModal() {
 	myModal.style.display = "flex";
+	allDocBackground.style.opacity = "0.5";
+	imgCollectModal;
 }
 
 export async function closeModal() {
 	myModal.style.display = "none";
+	allDocBackground.style.opacity = "1";
 }
+
+// test
+const imgCollectModal = fetch("http://localhost:5678/api/works")
+	.then((reponse) => reponse.json())
+	.then((data) => {
+		data.forEach((element) => {
+			// créer balises pour accueillir les images du portfolio
+			const modalGallery = document.getElementById("modalGallery");
+			const maDiv = document.createElement("div");
+			const myImg = document.createElement("img");
+
+			myImg.src = element.imageUrl;
+			maDiv.dataset.categorie = element.categoryId;
+
+			maDiv.classList.add("work-modal");
+
+			modalGallery.appendChild(maDiv);
+			maDiv.appendChild(myImg);
+		});
+	});
