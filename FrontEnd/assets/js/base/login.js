621081c1e229eee009b4a6c1b49e849ae2787010
Phanttoms <yohan.pruche@orange.fr>
2023-12-18 10:45:24 +0100

    ajout regex pour email et password

--- FrontEnd/assets/js/base/login.js
+++ FrontEnd/assets/js/base/login.js
@@ -2,34 +2,47 @@
 const loginForm = document.getElementById("formContent");
 const errorMessage = document.getElementById("error");
 
+// Variable Regex pour le Mail et le mot de passe
+const regexMail =
+	/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const regexPassword = /^[a-zA-Z]\w{5,14}/;
+
 // Fonction d'envoie des information de connection et verification de la réponse
 async function login(email, password) {
-	try {
-		const response = await fetch("http://localhost:5678/api/users/login", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				Accept: "application/json",
-			},
-			body: JSON.stringify({
-				email: email,
-				password: password,
-			}),
-		});
+	if (!regexMail.test(email)) {
+		errorMessage.textContent = "E-mail invalide";
+	} else if (!regexPassword.test(password)) {
+		errorMessage.textContent =
+			"Le mot de passe doit contenir au moins 6 caractere, dont un chiffre";
+	} else {
+		try {
+			const response = await fetch("http://localhost:5678/api/users/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+				},
+				body: JSON.stringify({
+					email: email,
+					password: password,
+				}),
+			});
 
-		if (response.status === 200) {
-			const data = await response.json();
-			const token = data.token;
-			localStorage.setItem("token", token);
-			window.location.href = "./index.html";
-			console.log(token);
-		} else {
-			errorMessage.textContent = "Erreur dans l’identifiant ou le mot de passe";
-			console.log(`Erreur: ${response.status} !`);
+			if (response.status === 200) {
+				const data = await response.json();
+				const token = data.token;
+				localStorage.setItem("token", token);
+				window.location.href = "./index.html";
+				console.log(token);
+			} else {
+				errorMessage.textContent =
+					"Erreur dans l’identifiant ou le mot de passe";
+				console.log(`Erreur: ${response.status} !`);
+			}
+		} catch (error) {
+			console.log("Erreur lors de la requête.");
+			console.log(error);
 		}
-	} catch (error) {
-		console.log("Erreur lors de la requête.");
-		console.log(error);
 	}
 }
 
